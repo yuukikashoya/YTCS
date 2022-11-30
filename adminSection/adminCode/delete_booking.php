f94c97486b017648117737ec0cc2a65d3317f059
johnlloyd <yukimiranda26@gmail.com>
2022-11-30 10:31:23 +0800

    booking edit

---
+++ adminSection/adminCode/delete_booking.php
@@ -0,0 +1,21 @@
+<?php
+
+include"../../database/connecting_to_DB.php";
+if(isset($_GET['service_id'])){
+	
+	$service_id = $_GET['service_id'];
+
+	$query = "DELETE FROM service WHERE service_id = '$service_id' ";
+
+	if($conn->query($query) === TRUE){
+
+        header('Location: ../adminWebsite/display.php?message=delete Successfully');
+		
+	}else{
+		
+		// echo"Error!!!";
+		echo '<script>window.alert("ERROR!")</script>';
+	}
+	
+}
+?>
