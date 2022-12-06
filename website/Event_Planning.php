491e8895336690d442142ab6d5107deebf0a80f0
johnlloyd <yukimiranda26@gmail.com>
2022-12-06 23:24:58 +0800

    temp

--- website/Event_Planning.php
+++ website/Event_Planning.php
@@ -234,39 +234,61 @@ celebrant itself won't needed much to hassle for the fulfillment of their dream 
     people to whom you care for. Many a time, when you think of expressing your feelings 
     <br>while writing a birthday message to a friend or family member, your mind goes blank 
     <br>and you are unable to fetch the most suitable words.</p></center>
-  <div class="lab">
-    <div class="packs1">
-      <p>The ideal catering package for soon-to-wed couples for a minimum of 100 guests, which includes:</p>
-      <li>Full-Service Catering</li>
-      <li>Reception Set-Up and Design</li>
-      <li>An Events Planner is assigned to facilitate the planning and execution of your event</li>
-      <li>Choice of complimentary wedding essentials</li>
-      <h3>This package starts at<span>P 1,300 per head</span></h3>
-    </div>
-    <br>
-    <div class="packs1">
-      <p>The ideal catering package for soon-to-wed couples for 
-      <br>a minimum of 100 guests, which includes:</p> 
-      <li>Full-Service Catering</li>
-      <li>Reception Set-Up and Design</li>
-      <li>An Events Planner is assigned to facilitate the<br> planning and execution of your event</li>
-      <li>Choice of complimentary wedding essentials</li>
-      <h3>This package starts at P 1,100 per head</h3> 
-    </div>
-    <br> 
-    <div class="packs1">
-      <p>The ideal catering package for soon-to-wed couples for a minimum of 100 guests, which includes:</p>
-      <li>Full-Service Catering</li>
-      <li>Reception Set-Up and Design</li>
-      <li>An Events Planner is assigned to facilitate the planning and execution of your event</li>
-      <li>Choice of complimentary wedding essentials</li>
-      <h3>This package starts at<span>P 1,300 per head</span></h3>
-    </div><br><br><br>
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  <a class="buttonm text-white "href="booking.php">Book Now</a>
-    <div class="packs1" style="background:transparent;">
-    
-    </div>
+    <div class="lab">
+<div class="packs1">
+    <p>   <b>Set A</b> <br>   The ideal catering food package for soon-to-wed couples for a maximum of 180 guests, which includes:</p>
+    <li>Beef Menu, Chicken Menu, Pork Menu, Fish Menu, and SeaFood Menu</li>
+    <li>Pasta And Vegestable</li>
+    <li>2 Dessert Salad</li>
+    <li>Soup</li>
+    <li>Rice</li>
+    <li>Drinks (Coke,Sprite,Pepsi,Rc)</li>
+    <h3>This package starts at ₱ 600 per head</h3>
   </div>
+  <br>
+  <div class="packs1">
+    <p>   <b>Set B</b> <br>   The ideal catering food package for soon-to-wed couples for a maximum of 180 guests, which includes:</p>
+    <li>Beef Menu, Chicken Menu, Pork Menu, Fish Menu, and SeaFood Menu</li>
+    <li>Pasta And Vegestable</li>
+    <li>2 Dessert Salad</li>
+    <li>Soup</li>
+    <li>Rice</li>
+    <li>Drinks (Coke,Sprite,Pepsi,Rc)</li>
+    <h3>This package starts at ₱ 600 per head</h3>
+  </div>
+  <br> 
+  <div class="packs1">
+    <p>   <b>Set B</b> <br>   The ideal catering food package for soon-to-wed couples for a maximum of 180 guests, which includes:</p>
+    <li>Beef Menu, Chicken Menu, Pork Menu, Fish Menu, and SeaFood Menu</li>
+    <li>Pasta And Vegestable</li>
+    <li>2 Dessert Salad</li>
+    <li>Soup</li>
+    <li>Rice</li>
+    <li>Drinks (Coke,Sprite,Pepsi,Rc)</li>
+    <h3>This package starts at ₱ 600 per head</h3>
+  </div>
+  <br>
+  <div class="packs1">
+    <p>   <b>Set B</b> <br>   The ideal catering food package for soon-to-wed couples for a maximum of 180 guests, which includes:</p>
+    <li>Beef Menu, Chicken Menu, Pork Menu, Fish Menu, and SeaFood Menu</li>
+    <li>Pasta And Vegestable</li>
+    <li>2 Dessert Salad</li>
+    <li>Soup</li>
+    <li>Rice</li>
+    <li>Drinks (Coke,Sprite,Pepsi,Rc)</li>
+    <h3>This package starts at ₱ 600 per head</h3>
+  </div>  <br>
+  <div class="packs1">
+<li>Note!! price will varie from its maximum range</li>
+
+</div>
+  <br>
+  <br><br>
+  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  <a class="buttonm text-white " href="booking.php">Book Now</a>
+  <div class="packs1" style="background:transparent;">
+  
+  </div>
+</div>
   </div>
 </body>
 
